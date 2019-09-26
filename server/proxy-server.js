9198106952ab4af554a310eaa5dc943a56e365bc
Jesse Forrester <jesse.forrester@yahoo.com>
2019-09-26 15:43:54 -0700

    adjustments to server file

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -3,14 +3,11 @@
 const newRelic = require('newrelic');
 const express = require('express');
 const axios = require('axios');
-// const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = 3000;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/:id/', express.static('public'));
 
 app.get('/api/:listing/photos', (req, res) => {
